6a5f2491a4fbc3099fc13f85db034e357394b54b
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-02-17 23:43:47 +0100

    setup-user.sh: add default user to more groups
    In order to gain access to some of the phone's hardware such as the 
    camera or soundcard, we need to add the default user to a few more 
    groups.

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -4,6 +4,13 @@ USERNAME=$1
 
 adduser --gecos $USERNAME --disabled-password --shell /bin/bash $USERNAME
 adduser $USERNAME sudo
+
+# Needed for hardware access rights
+adduser $USERNAME video
+adduser $USERNAME audio
+adduser $USERNAME bluetooth
+adduser $USERNAME plugdev
+
 echo "$USERNAME:1234" | chpasswd
 echo "root:root" | chpasswd
 
