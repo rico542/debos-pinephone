3ac8e5dc71a463480f66fe80695f096802b136fa
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-02-24 11:45:35 +0100

    update u-boot & boot script
    A new u-boot version has been compiled for better visual feedback during 
    boot. As such, the boot script has to be updated to take into account 
    these changes.

--- overlays/rootfs/boot/boot.cmd
+++ overlays/rootfs/boot/boot.cmd
@@ -1,15 +1,14 @@
 setenv bootargs console=tty0 console=ttyS0,115200 no_console_suspend panic=10 consoleblank=0 loglevel=7 root=/dev/mmcblk0p1 rw quiet splash
 
-gpio set 114
+led 1 on
 echo "Loading kernel..."
 load mmc 0:1 ${ramdisk_addr_r} /boot/Image.gz
 echo "Uncompressing kernel..."
 unzip ${ramdisk_addr_r} ${kernel_addr_r}
-gpio set 115
 echo "Loading initramfs..."
 load mmc 0:1 ${ramdisk_addr_r} /boot/initrd.uImg
 echo "Loading dtb..."
 load mmc 0:1 ${fdt_addr_r} /boot/dtb/${fdtfile}
-gpio set 116
+led 2 on
 echo "Booting..."
 booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
