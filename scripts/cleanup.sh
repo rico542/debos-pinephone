690589866a67985d71196f404908f30776958195
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-02-26 16:48:56 +0100

    cleanup.sh: remove unnecessary packages & cleanup archives

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,3 +6,8 @@ rm /boot/initrd.img
 # Remove unnecessary .desktop files
 rm /usr/share/applications/vim.desktop \
    /usr/share/applications/htop.desktop
+
+# Remove apt packages which are no longer unnecessary and delete
+# downloaded packages
+apt -y autoremove
+apt clean
