93c10b6a3a9c276d6d122c816ab6fe86e66332a9
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-02-26 19:16:00 +0100

    build.sh: improve build script
    With this patch, the build script can handle a number of command-line 
    options, allowing to set:
    - the default username and password
    - the http and/or ftp proxy
    
    It also allows to build only the image recipe (provided the user have 
    previously run the rootfs recipe) and to optionnally compress the 
    generated image.

--- build.sh
+++ build.sh
@@ -1,18 +1,49 @@
 #!/bin/sh
 
 export PATH=/sbin:/usr/sbin:$PATH
+IMG_FILE="debian-pinephone-`date +%Y%m%d`.img"
+ARGS=
+image_only=
+do_compress=
+username=
+password=
 
-if [ "$http_proxy" ]
-then
-  ARGS="-e http_proxy:\"$http_proxy\""
+while getopts "izup:h:f:" opt
+do
+  case "$opt" in
+    i ) image_only=1 ;;
+    z ) do_compress=1 ;;
+    u ) username="$OPTARG" ;;
+    p ) password="$OPTARG" ;;
+    h ) http_proxy="$OPTARG" ;;
+    f ) ftp_proxy="$OPTARG" ;;
+  esac
+done
+
+if [ "$username" ]; then
+  ARGS="$ARGS -t username:\"$username\""
 fi
 
-if [ "$ftp_proxy" ]
-then
+if [ "$password" ]; then
+  ARGS="$ARGS -t password:\"$password\""
+fi
+
+if [ "$http_proxy" ]; then
+  ARGS="$ARGS -e http_proxy:\"$http_proxy\""
+fi
+
+if [ "$ftp_proxy" ]; then
   ARGS="$ARGS -e ftp_proxy:\"$ftp_proxy\""
 fi
 
 ARGS="$ARGS --scratchsize=8G"
 
-debos $ARGS rootfs.yaml &&
-debos $ARGS image.yaml
+if [ ! "$image_only" ]; then
+  debos $ARGS rootfs.yaml || exit 1
+fi
+debos $ARGS -t image:$IMG_FILE image.yaml
+
+if [ "$do_compress" ]; then
+  echo "Compressing $IMG_FILE..."
+  gzip --keep --force $IMG_FILE
+fi
