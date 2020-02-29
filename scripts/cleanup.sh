a414ed6638a59c8a62bbca42743eccc9131bb561
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-02-29 01:29:12 +0100

    cleanup.sh: make sure the script won't fail
    In case these recipes are used to create a custom image, some of the 
    files we remove in the cleanup script might not be present, leading to a 
    build failure.
    
    In order to prevent this, we now use `rm -f`, which should never fail.
    
    Additionally, purge the packages during the autoremove.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Remove initrd as it has been converted to uImage
-rm /boot/initrd.img
+rm -f /boot/initrd.img
 
 # Remove unnecessary .desktop files
-rm /usr/share/applications/vim.desktop \
-   /usr/share/applications/htop.desktop
+rm -f /usr/share/applications/vim.desktop \
+      /usr/share/applications/htop.desktop
 
 # Remove apt packages which are no longer unnecessary and delete
 # downloaded packages
-apt -y autoremove
+apt -y autoremove --purge
 apt clean
