fd3d6e926733f763f94d176a71ac02cbb3a7b357
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-02-29 18:14:44 +0100

    boot.cmd: make sure we can boot from both SD and the eMMC

--- overlays/rootfs/boot/boot.cmd
+++ overlays/rootfs/boot/boot.cmd
@@ -1,14 +1,28 @@
-setenv bootargs console=ttyS0,115200 no_console_suspend panic=10 consoleblank=0 loglevel=7 root=/dev/mmcblk0p1 rw splash plymouth.ignore-serial-consoles
+if test ${mmc_bootdev} -eq 0 ; then
+	echo "Booting from SD";
+	setenv linux_mmcdev 0;
+else
+	echo "Booting from eMMC";
+	setenv linux_mmcdev 2;
+fi;
+
+setenv bootargs console=ttyS0,115200 no_console_suspend panic=10 consoleblank=0 loglevel=7 root=/dev/mmcblk${linux_mmcdev}p1 rw splash plymouth.ignore-serial-consoles
 
 led 1 on
+
 echo "Loading kernel..."
-load mmc 0:1 ${ramdisk_addr_r} /boot/Image.gz
+load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} /boot/Image.gz
+
 echo "Uncompressing kernel..."
 unzip ${ramdisk_addr_r} ${kernel_addr_r}
+
 echo "Loading initramfs..."
-load mmc 0:1 ${ramdisk_addr_r} /boot/initrd.uImg
+load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} /boot/initrd.uImg
+
 echo "Loading dtb..."
-load mmc 0:1 ${fdt_addr_r} /boot/dtb/${fdtfile}
+load mmc ${mmc_bootdev}:1 ${fdt_addr_r} /boot/dtb/${fdtfile}
+
 led 2 on
+
 echo "Booting..."
 booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
